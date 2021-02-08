e9b45c9940606fac33f25dc491febcd9105011c1
Stepan <st085873@gmail.com>
2021-02-08 16:43:17 +0300

    Figures fabric first implementation

--- src/figures/FigureFabric.kt
+++ src/figures/FigureFabric.kt
@@ -1,5 +1,21 @@
 package figures
 
-class FigureFabric {
+import java.util.Vector
 
+class FigureFabric(private var figures: Vector<BaseFigure> = Vector()) {
+    init {
+        if(figures.isEmpty()) {
+            figures.addElement(IFigure())
+            figures.addElement(JFigure())
+            figures.addElement(LFigure())
+            figures.addElement(OFigure())
+            figures.addElement(SFigure())
+            figures.addElement(TFigure())
+            figures.addElement(ZFigure())
+        }
+    }
+
+    fun getRandomFigure(): BaseFigure {
+        return figures[(Math.random() * figures.size).toInt()].clone() as BaseFigure
+    }
 }
