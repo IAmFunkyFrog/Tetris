0efc90fb54082c4e2d4d3cd6cf76ad2df5407c2c
Stepan <st085873@gmail.com>
2021-02-09 22:43:46 +0300

    Demo game

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,27 @@
-fun main() {
-    val game = Game(5, 10)
+import kotlinx.coroutines.*
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-    for(row in game.table) {
-        for(cell in row) print(cell)
+fun moveFigure(move: Char, game: Game) {
+    when(move) {
+        'a' -> game.figureMoveLeft()
+        'd' -> game.figureMoveRight()
+        'w' -> game.figureRotateLeft()
+        's' -> game.figureRotateRight()
+        'e' -> game.tick()
+    }
+}
+
+fun main(args: Array<String>) {
+    val game = Game(8, 14)
+    game.restart()
+    while(game.inProcess) {
+        val move: String? = readLine()
+        if(move != null) moveFigure(move[0], game)
+        for(i in game.table.size - 1 downTo 0) {
+            for(j in game.table[i].indices) print(game.table[i][j])
+            println()
+        }
         println()
     }
 }
